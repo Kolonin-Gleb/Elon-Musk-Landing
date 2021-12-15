eff0e720c2a20456c837e4f10a686e3169e8bb53
Kolonin-Gleb <topsecretway@yandex.ru>
2021-12-15 08:13:20 +0300

    Styles for map and JS were added
    Map styles were added.
    Styles for Pop-up were added.
    JS for Pop-up was added.

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
-// let today = new Date();
-// alert("Today's date is " + today);
+/* Pop Up for Interactive map */
 
+let continents = document.querySelectorAll('.continent');
+let popupBg = document.querySelector('.info__bg');
+let popup__photo = document.querySelector('.info__photo');
+let popup__title = document.querySelector('.info__title');
+let popup__text = document.querySelector('.info__text');
+let tooltip = document.querySelector('.tooltip');
+
+continents.forEach((item) => {
+    item.addEventListener('click', function() {
+        popup__title.textContent = this.getAttribute('data-title');
+        popup__photo.setAttribute('src', this.getAttribute('data-photo'));
+        popup__text.textContent = this.getAttribute('data-text');
+        popupBg.classList.add('active');
+    });
+
+    item.addEventListener('mouseenter', function() {
+        tooltip.textContent = item.getAttribute('data-title');
+        tooltip.style.display = 'block';
+    });
+
+    item.addEventListener('mouseleave', function() {
+        tooltip.textContent = item.getAttribute('data-title');
+        tooltip.style.display = 'none';
+    });
+
+    item.addEventListener('mousemove', function(e) {
+        tooltip.style.top = (e.y + 20) + 'px';
+        tooltip.style.left = (e.x + 20) + 'px';
+    });
+});
+
+document.addEventListener('click', (e) => {
+    if(e.target === popupBg)
+    {
+        popupBg.classList.remove('active');
+    }
+});
